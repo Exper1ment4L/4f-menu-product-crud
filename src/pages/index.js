6a33d4c80e692d41e05756d75b2c41da80fbb96f
Berkay Burak <bbm@users.noreply.github.com>
2019-09-30 16:24:31 +0300

    BBM

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import TextField from '../components/TextField.js';
-import ReactDOM from 'react-dom';
 import Button from '../components/Button.js'
 
 const Home = () => (
   <div>
-   <h1>Adding Product</h1>
+    <h1>Add Product</h1>
    <TextField />
    <Button>Add</Button>
   </div>
