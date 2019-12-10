eeaa1853e95c2a989ca212adab56138e1299481b
Berkay Burak <Exper1ment4L@users.noreply.github.com>
2019-12-10 19:19:15 +0300

    db key

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://localhost/4f',
+  mongoURI: 'mongodb+srv://admin:123@cluster0-ijzqe.mongodb.net/test?retryWrites=true&w=majority',
   apiKEY: 'SECRET_API_KEY',
 };
